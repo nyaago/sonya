c8c99435d192c25161da97b106ea51ba494b2c3a
nyaago <nyaago69@mac.com>
2014-01-13 18:29:07 +0900

    Initial. Creates Rails Application. Implements Authorization.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sonya::Application.config.secret_key_base = '27474ccd3f94ee47dbe34f44f52815067b79373f037195ea3f0517bd601524c10ffd33b25a9b4897cf3c2041da825e81913337fb681ec8c738a8656eb82d8a6f'
---
+++ db/create_mysql_db.sql
@@ -0,0 +1,4 @@
+create database sonya
+default character set utf8;
+
+GRANT ALL PRIVILEGES ON sonya.* TO moomin@localhost IDENTIFIED BY 'moomin' WITH GRANT OPTION;
